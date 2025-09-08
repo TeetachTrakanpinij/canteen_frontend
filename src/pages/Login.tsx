653a380d5e07a77999b6121553356306d224449a
TeetachTrakanpinij <male7447@gmail.com>
2025-09-08 09:03:00 +0700

    male test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -54,7 +54,7 @@ export default function Login() {
         </button>
       </div>
 
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen px-8">
         <div className="bg-white w-full max-w-md rounded-2xl shadow-lg p-8">
           <h1 className="text-2xl font-bold text-center mb-6">
             Smart Canteen Log In
